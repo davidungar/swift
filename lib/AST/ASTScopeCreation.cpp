03f04791df7eed7dda656f0b69f303b9c9434251
David Ungar <dungar@apple.com>
2019-08-10 17:43:15 -0700

    Fix some no-assert warnings

--- lib/AST/ASTScopeCreation.cpp
+++ lib/AST/ASTScopeCreation.cpp
@@ -82,6 +82,8 @@ static void dumpRangeable(const SpecializeAttr *r,
 static void dumpRangeable(const SpecializeAttr *r, llvm::raw_ostream &f) {
   llvm::errs() << "SpecializeAttr\n";
 }
+static void dumpRangeable(SpecializeAttr *r,
+                          llvm::raw_ostream &f) LLVM_ATTRIBUTE_USED;
 static void dumpRangeable(SpecializeAttr *r, llvm::raw_ostream &f) {
   llvm::errs() << "SpecializeAttr\n";
 }
